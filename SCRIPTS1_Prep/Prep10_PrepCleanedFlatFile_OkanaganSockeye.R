0f399133f00db7976c29593ac82dcc0ffb339039
SOLV-Code <30556192+SOLV-Code@users.noreply.github.com>
2023-10-07 09:07:09 -0700

    Okanagan Sk prep fix

--- SCRIPTS1_Prep/Prep10_PrepCleanedFlatFile_OkanaganSockeye.R
+++ SCRIPTS1_Prep/Prep10_PrepCleanedFlatFile_OkanaganSockeye.R
@@ -13,22 +13,21 @@ names(cu.info.main)
 ok.sk.src <- left_join(ok.sk.src, cu.info.main %>% select(CU_Name,CU_ID,DU_ID) )
 ok.sk.src
 
+
 ok.sk.clean <- ok.sk.src %>%
                                               mutate(Species = "Sockeye",
                              SpnForAbd_Total	 = Spn,
                              SpnForTrend_Total	 = Spn,
                              SpnForAbd_Wild		 = Spn,
                              SpnForTrend_Wild	 = Spn,
-                             STK_ID = CU_ID,
-
-                             ) %>%
+                             STK_ID = CU_ID,	STK_NAME = CU_Name ) %>%
                         select(Species,	Year,	SpnForAbd_Total,	SpnForTrend_Total,	SpnForAbd_Wild,
                           SpnForTrend_Wild,	CU_ID,	CU_Name,	DU_ID,	STK_ID,	STK_NAME)
 
 head(ok.sk.clean)
 
 
-write.csv(ok.sk.clean, "DATA_PROCESSING/Cleaned_FlatFile_ByCU_OkanagansSockeye.csv",row.names=FALSE)
+write.csv(ok.sk.clean, "DATA_PROCESSING/Cleaned_FlatFile_ByCU_OkanaganSockeye.csv",row.names=FALSE)
 
 
 
