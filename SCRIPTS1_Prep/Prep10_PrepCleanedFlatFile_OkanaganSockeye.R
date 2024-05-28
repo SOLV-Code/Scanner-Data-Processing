82bb2c530bd73b1cebb5ac5c5eff9ff56a2cfaa6
SOLV-Code <30556192+SOLV-Code@users.noreply.github.com>
2024-05-27 21:04:32 -0700

    data processing script cross-check done

--- SCRIPTS1_Prep/Prep10_PrepCleanedFlatFile_OkanaganSockeye.R
+++ SCRIPTS1_Prep/Prep10_PrepCleanedFlatFile_OkanaganSockeye.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 
 
+
+if(FALSE){ # START REGULAR VERSION SKIP
+
+
 ok.sk.src <- read_csv("DATA_IN/SOURCES/Okanagan Sockeye/GENERATED_SR_Data_Osoyoos.csv") %>%
                 dplyr::rename(Spn = "Spawners") %>% 
                 mutate(CU_Name = "Osoyoos-L")
@@ -29,7 +33,46 @@ head(ok.sk.clean)
 
 write.csv(ok.sk.clean, "DATA_PROCESSING/Cleaned_FlatFile_ByCU_OkanaganSockeye.csv",row.names=FALSE)
 
+} # END REGULAR VERSIONS
 
 
+
+
+if(TRUE){ # START Alternative SCENARIOS VERSION
+  
+  
+  ok.sk.src <- read_csv("DATA_IN/SOURCES/Okanagan Sockeye/OkanaganSk_AltScenarios.csv") %>% 
+    pivot_longer(-Year,names_to = "CU_ID",values_to = "Spn") %>%
+    select(CU_ID,everything()) %>% arrange(CU_ID, Year)
+  
+  
+  
+  
+  head(ok.sk.src )
+  
+  
+  cu.info.main <- read_csv("DATA_LOOKUP_FILES/MOD_MAIN_CU_LOOKUP_FOR_SOS.csv")
+  names(cu.info.main)
+  
+  ok.sk.src <- left_join(ok.sk.src, cu.info.main %>% select(CU_Name,CU_ID,DU_ID),by="CU_ID" )
+  ok.sk.src
+  
+  
+  ok.sk.clean <- ok.sk.src %>%
+    mutate(Species = "Sockeye",
+           SpnForAbd_Total	 = Spn,
+           SpnForTrend_Total	 = Spn,
+           SpnForAbd_Wild		 = Spn,
+           SpnForTrend_Wild	 = Spn,
+           STK_ID = CU_ID,	STK_NAME = CU_Name ) %>%
+    select(Species,	Year,	SpnForAbd_Total,	SpnForTrend_Total,	SpnForAbd_Wild,
+           SpnForTrend_Wild,	CU_ID,	CU_Name,	DU_ID,	STK_ID,	STK_NAME)
+  
+  head(ok.sk.clean)
+  
+  
+  write.csv(ok.sk.clean, "DATA_PROCESSING/Cleaned_FlatFile_ByCU_OkanaganSockeye.csv",row.names=FALSE)
+  
+} # END OK SK CSAS SCENARIOS VERSION
 
 
