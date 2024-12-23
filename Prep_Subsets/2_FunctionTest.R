22cab2264dee8916a6a7fb8d64b96428baa4cbe4
SOLV-Code <gpestal@solv.ca>
2024-12-23 11:03:22 -0800

    debugging calculate metrics by CU function

--- Prep_Subsets/2_FunctionTest.R
+++ Prep_Subsets/2_FunctionTest.R
@@ -15,10 +15,10 @@ source("Prep_Subsets/Tmp_Functions/2_CalculateMetrics_By_CU_FUNCTION.R")
 # Run on "Worked Examples" set
 
 worked.examples.metrics.out <- calculateMetricsByCU(cu.file = read_csv("Prep_Subsets/Output/WorkedExamples/CU_Data_WorkedExamples.csv"),  
-                                    cu.info = read_csv("Prep_Subsets/Output/WorkedExamples/CU_Specs_WorkedExamples.csv")     ,
-                                    cyclic.cu.bm = NULL,
-                                 retro.start.use = 1995,
-                                 out.filepath = "Prep_Subsets/Output/WorkedExamples/RetrospectiveMetrics_Output.csv")
+                               cu.info = read_csv("Prep_Subsets/Output/WorkedExamples/CU_Specs_WorkedExamples.csv")     ,
+                               cyclic.cu.bm = NULL,
+                               retro.start.use = 1995,
+                               out.filepath = "Prep_Subsets/Output/WorkedExamples/RetrospectiveMetrics_Output.csv")
 
 
 
