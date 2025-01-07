6e1337b3a43dd1cb1709e68e671a2f52636ee148
SOLV-Code <gpestal@solv.ca>
2025-01-07 12:34:06 -0800

    fixed CU_ID issue in functions

--- Prep_Subsets/2_FunctionTest.R
+++ Prep_Subsets/2_FunctionTest.R
@@ -13,7 +13,11 @@ source("Prep_Subsets/Tmp_Functions/Module_generateRapidStatus.R")
 source("Prep_Subsets/Tmp_Functions/Module_plotStatusDashboards.R")
 source("Prep_Subsets/Tmp_Functions/Module_generateReportTemplate.R")
 
+
+
+############################################################################################
 # Run on "Worked Examples" set
+############################################################################################
 
 worked.examples.metrics.out <- calculateMetricsByCU(cu.file = read_csv("Prep_Subsets/Output/WorkedExamples/CU_Data_WorkedExamples.csv"),  
                                cu.info = read_csv("Prep_Subsets/Output/WorkedExamples/CU_Specs_WorkedExamples.csv")     ,
@@ -42,6 +46,7 @@ names(worked.examples.metrics.out$Data)
 names(rapid.status.input$Status)
 names(rapid.status.input$Values)
 
+source("Prep_Subsets/Tmp_Functions/Module_generateRapidStatus.R")
 rapid.status.results <- generateRapidStatus(cu.info = worked.examples.metrics.out$Specs,
                                 cu.data = worked.examples.metrics.out$Data,
                                 publ.status.src = read_csv("Prep_Subsets/Output/WorkedExamples/CU_PublishedIntegratedStatuses_WorkedExamples.csv"),  
@@ -83,3 +88,90 @@ generateReportTemplate(type = "quarto",
                        file.label = "Report", # doesn't apply to readme
                        files.path = "Prep_Subsets/Output/WorkedExamples", 
                        repo.path = NULL)
+
+
+
+
+
+
+
+############################################################################################
+# Run on "Fraser Sandbox" set
+############################################################################################
+
+fraser.sandbox.metrics.out <- calculateMetricsByCU(cu.file = read_csv("Prep_Subsets/Output/FraserSandbox/CU_Data_FraserSandbox.csv"),  
+                                                    cu.info = read_csv("Prep_Subsets/Output/FraserSandbox/CU_Specs_FraserSandbox.csv")     ,
+                                                    cyclic.cu.bm = NULL,
+                                                    retro.start.use = 1995,
+                                                    out.label = "FraserSandbox",
+                                                    out.filepath = "Prep_Subsets/Output/FraserSandbox")
+names(fraser.sandbox.metrics.out)
+
+
+names(fraser.sandbox.metrics.out$Metrics)
+
+rapid.status.input <- prepDataForRapidStatus(
+  cu.info = fraser.sandbox.metrics.out$Specs,
+  metrics.raw = fraser.sandbox.metrics.out$Metrics,
+  gen.avg.src = fraser.sandbox.metrics.out$GenAvg, 
+  out.label = "FraserSandbox",
+  out.filepath = "Prep_Subsets/Output/FraserSandbox")
+
+names(rapid.status.input)
+head(rapid.status.input$Values)
+
+
+names(fraser.sandbox.metrics.out$Specs)
+names(fraser.sandbox.metrics.out$Data)
+names(rapid.status.input$Status)
+names(rapid.status.input$Values)
+
+sort(unique(rapid.status.input$Values$CU_ID))
+
+
+source("Prep_Subsets/Tmp_Functions/Module_applyRapidStatusTree.R")
+source("Prep_Subsets/Tmp_Functions/Module_generateRapidStatus.R")
+
+rapid.status.results <- generateRapidStatus(cu.info = fraser.sandbox.metrics.out$Specs,
+                                            cu.data = fraser.sandbox.metrics.out$Data,
+                                            publ.status.src = read_csv("Prep_Subsets/Output/FraserSandbox/CU_PublishedIntegratedStatuses_FraserSandbox.csv"),  
+                                            retro.values = rapid.status.input$Values, 
+                                            retro.status = rapid.status.input$Status,
+                                            metrics.long = rapid.status.input$LongForm,
+                                            group.var = "Species",
+                                            out.label = "FraserSandbox",
+                                            out.filepath = "Prep_Subsets/Output/FraserSandbox")
+names(rapid.status.results)
+rapid.status.results$Summary
+rapid.status.results$SummaryTable
+rapid.status.results$Data
+rapid.status.results$Rules
+rapid.status.results$ConfusionMatrix
+rapid.status.results$RawConfusionMatrix
+rapid.status.results$Accuracy
+
+range(rapid.status.results$Data$Year)
+
+
+
+plotStatusDashboards(
+  cu.info =  fraser.sandbox.metrics.out$Specs,
+  retro.summary.tbl = rapid.status.results$SummaryTable,
+  cu.data = fraser.sandbox.metrics.out$Data, 
+  out.label = "RapidStatusOut",
+  out.filepath = "Prep_Subsets/Output/FraserSandbox/Dashboards")
+
+
+source("Prep_Subsets/Tmp_Functions/Module_generateReportTemplate.R")
+generateReportTemplate(type = "readme", 
+                       file.label = "Report", # doesn't apply to readme
+                       files.path = "Prep_Subsets/Output/FraserSandbox", 
+                       repo.path = "https://github.com/SOLV-Code/Scanner-Data-Processing/tree/main/")
+
+
+generateReportTemplate(type = "quarto", 
+                       file.label = "Report", # doesn't apply to readme
+                       files.path = "Prep_Subsets/Output/FraserSandbox", 
+                       repo.path = NULL)
+
+
